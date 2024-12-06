7ba0c50d8eda654654928f80751a9814b54f2a22
rvaghela <rvaghela@lumosfinserv.com>
2024-12-06 13:18:12 +0530

    header ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     
     <div>
-      test
+      <Header />
     </div>
   );
 }
