e522fdcfd1cc552aabf0738b0c61efb41e41e039
Rakesh Patel <rakepa84@gmail.com>
2024-12-10 14:58:22 +0530

    explore section and product list ui implementation

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,17 @@ import Header from "./components/Header";
 import CategoryItem from './components/CategoryItem';
 import StayConnected from "./components/StayConnected";
 import Footer from "./components/Footer";
+import ExploreSection from "./components/ExploreSection";
+import SellingProductOfMonth from "./components/SellingProductOfMonth";
 
 export default function Home() {
   return (
     
     <div>
       <Header />
-      <CategoryItem />
+      {/* <CategoryItem /> */}
+      <ExploreSection />
+      <SellingProductOfMonth />
       <StayConnected />
       <Footer />
     </div>
